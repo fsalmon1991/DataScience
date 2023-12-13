0c9a656626205f8449bb59bd7ffb6e43809fffb2
Francisco Salmon <fsalmon1991@gmail.com>
2023-12-13 10:53:04 -0600

    added methods to sort the data frame and get the final result as a dict with the points coodinates as keys and, frequency and amplitude as values

--- tools.py
+++ tools.py
@@ -6,6 +6,9 @@ import pandas as pd
 import geopandas
 # Reading the dataset
 df = pd.read_csv('/Users/franciscosalmonmoret/Documents/Study/DataScience/NDMaI_1990_1994.csv')
+rows = int(df.shape[0]/2)
+print(rows)
+df = df.iloc[:rows,:]
 
 # Making a new dataframe for the geodata
 df_geom = df[["geometry"]].copy()
@@ -18,12 +21,28 @@ def dltinvalid(value,data):
     for elements in data:
         if elements != -10:
             clean_data.append(elements)
-    return clean_data        
+    return clean_data  
 
-
+def orderArraybyOIndex(arrayA,arrayB):
+    
+    listaA = arrayA.tolist()
+    listaB = arrayB.tolist()
+    lista_index = []
+    for elements in listaA:
+        lista_index.append((elements, listaA.index(elements)))
+    sortedA = np.sort(arrayA)[::-1]
+    result = []
+    for data in sortedA:
+        for value in lista_index:
+            if data == value[0]:
+                result.append((data, listaB[value[1]]))   
+    return result
 #Making a new dataframe for the data isolated from the geodata in the original dataframe by deleting the geometry column
 df_data = df.drop(columns=["geometry"],axis=1)
+# Splitting in two half the data frame and working the first half because the tow half are equals
 rowlist = df_data.values.tolist()
+
+
 row_full_size = len(rowlist)
 fft_result_dir = {}
 for data in rowlist:
@@ -32,7 +51,8 @@ for data in rowlist:
     row_clean_size = len(clean_data)
     print("This is the amount of  data after being cleaned in the rows = " + str(row_clean_size))
     fftout = fft.fft(clean_data)
-    data_with_sizes = len(data),row_clean_size,np.abs(fftout),fft.fftfreq(len(data))
+    data_with_sizes = len(data),row_clean_size,orderArraybyOIndex(fft.fftfreq(len(clean_data)),np.abs(fftout))
     fft_result_dir[tuple(geom_list[rowlist.index(data)])] = data_with_sizes
-
+    
+#sortedDict = sorted(fft_result_dir.items(), key=lambda x:x[1])
 pprint.pprint(fft_result_dir)
